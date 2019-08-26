7ca8e0f20701fa9059cd198124324019ab4dec89
CamilaRivera <camila.riverart@gmail.com>
2019-08-26 19:48:50 +0000

    assertArraysEqual refactor to include arrays in the output

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,14 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require("./eqArrays");
+const assertEqual = require("./assertEqual");
+
 
 const assertArraysEqual = function(array1, array2){
   if(eqArrays(array1,array2)){
-    console.log("Both arrays are equal");
+    console.log(`Both arrays are equal: ${array1} === ${array2}`);
   } else {
-    console.log("Both arrays are different");
+    console.log(`Both arrays are not equal: ${array1} !== ${array2}`);
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
 
