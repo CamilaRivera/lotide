9617d7f187f19cb5074add06987ed5515d07ecae
CamilaRivera <camila.riverart@gmail.com>
2019-08-26 19:55:23 +0000

    Refactor middle to include test

--- middle.js
+++ middle.js
@@ -1,23 +1,4 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log("Both arrays are equal");
-  } else {
-    console.log("Both arrays are different");
-  }
-};
+const assertArraysEqual =require("./assertArraysEqual");
 
 const middle = function (array) {
   let arrayMiddleTerm = [];
@@ -32,9 +13,4 @@ const middle = function (array) {
   return arrayMiddleTerm;
 }
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+module.exports = middle;
